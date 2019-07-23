2a52f6cf14f575698f0b7907c32dd2eb9fd88aaf
mani <sadhak001@gmail.com>
2019-07-23 15:04:41 +0100

    Rebuild image from scratch and remove components with vulnerabilities as reported  by docker vulnerability scanner

--- build/buildDockerImage.sh
+++ build/buildDockerImage.sh
@@ -10,5 +10,5 @@ DOCKER_FULL_IMAGE_NAME="meterianbot/meterian-scanner-docker:${VERSION}"
 echo "~~~~~~ Building the docker container for the Meterian Scanner client"
 docker build                                    \
        -t ${DOCKER_FULL_IMAGE_NAME}             \
-       --build-arg METERIAN_HOME=/meterian_home \
+       --build-arg METERIAN_HOME=/home/meterian  \
        .
