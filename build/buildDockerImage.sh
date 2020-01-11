adb86afdc6666599ba3cfda7e9d60466036201bc
Bruno Bossola <bbossola@gmail.com>
2020-01-11 19:42:30 +0000

    Added CircleCI build ID if present in the final packaged version displayed

--- build/buildDockerImage.sh
+++ build/buildDockerImage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
-set -u
 set -o pipefail
 
 VERSION="$(cat ../version.txt)"
 DOCKER_FULL_IMAGE_NAME="meterianbot/meterian-scanner-docker:${VERSION}"
+BUILD=${CIRCLE_BUILD_NUM:-000}
+VERSION_WITH_BUILD=${VERSION}.${BUILD}
 
 echo "~~~~~~ Building the docker container for the Meterian Scanner client"
-docker build -t ${DOCKER_FULL_IMAGE_NAME} --build-arg VERSION=${VERSION} .
+docker build -t ${DOCKER_FULL_IMAGE_NAME} --build-arg VERSION=${VERSION_WITH_BUILD} .
