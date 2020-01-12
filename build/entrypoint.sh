febcde88a2b4832c42376513a603b21936582357
Bruno Bossola <bbossola@gmail.com>
2020-01-12 18:27:35 +0000

    Now UID is optional

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 set -e
-set -u
 set -o pipefail
-
-# debugging users
-# echo "Host uid: ${HOST_UID}"
-# echo "Host gid: ${HOST_GID}"
-
-# create the user
-groupadd -g ${HOST_GID} meterian
-useradd -g meterian -u ${HOST_UID} meterian -d /home/meterian
-# grep meterian /etc/passwd
 
 # prepare the script file
 mv /root/meterian.sh /tmp/meterian.sh
 mv /root/version.txt /tmp/version.txt
-chmod +x /tmp/meterian.sh
 
-# launch meterian client with the newly created user
-export XPATH=$PATH
-su meterian -c -m /tmp/meterian.sh $* 2>/dev/null
+# run the script binding the user if required
+if [ "${HOST_UID}" == "" ];
+then
+    export XPATH=$PATH
+    /tmp/meterian.sh $*
+
+    # please do not add any command here as we need to preserve the exit status
+    # of the meterian client
+else
+    # create the user
+    echo UID: ${HOST_UID}
+    useradd -u ${HOST_UID} meterian -d /home/meterian
+
+    # launch meterian client with the newly created user
+    export XPATH=$PATH
+    su meterian -c -m /tmp/meterian.sh $* 2>/dev/null
+
+    # please do not add any command here as we need to preserve the exit status
+    # of the meterian client
+fi
 
 # please do not add any command here as we need to preserve the exit status
 # of the meterian client
