795209b346af331d945116d18574e7d1c7a71668
Bruno Bossola <bbossola@gmail.com>
2020-01-12 17:58:08 +0000

    Now pushing the 'latest' tag

--- build/buildDockerImage.sh
+++ build/buildDockerImage.sh
@@ -4,9 +4,10 @@ set -e
 set -o pipefail
 
 VERSION="$(cat ../version.txt)"
-DOCKER_FULL_IMAGE_NAME="meterian/cli:${VERSION}"
+DOCKER_IMAGE_NAME="meterian/cli"
+DOCKER_FULL_IMAGE_NAME="${DOCKER_IMAGE_NAME}:${VERSION}"
 BUILD=${CIRCLE_BUILD_NUM:-000}
 VERSION_WITH_BUILD=${VERSION}.${BUILD}
 
 echo "~~~~~~ Building the docker container for the Meterian Scanner client"
-docker build -t ${DOCKER_FULL_IMAGE_NAME} --build-arg VERSION=${VERSION_WITH_BUILD} .
+docker build -t ${DOCKER_FULL_IMAGE_NAME} -t ${DOCKER_IMAGE_NAME}:latest --build-arg VERSION=${VERSION_WITH_BUILD} .
