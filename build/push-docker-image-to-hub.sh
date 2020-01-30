81fb334ffe3fa41854c0cb96d6ec8ef6000194bd
Nana Insaidoo <nana.insaidoo@meterian.com>
2020-01-30 14:14:19 +0000

    Now blocking image push when version is already available on the docker repository

--- build/push-docker-image-to-hub.sh
+++ build/push-docker-image-to-hub.sh
@@ -20,6 +20,18 @@ findImage() {
 	echo $(docker images ${IMAGE_NAME} -q | head -n1 || true)
 }
 
+isImageVersionOnDockerHub() {
+    version=$1
+    
+    res=1
+    if [[ $(curl -o /dev/null -s -w "%{http_code}\n" "https://hub.docker.com/v2/repositories/meterian/cli-canary/tags/${version}") -eq 200 ]];
+    then
+        res=0
+    fi
+    
+    echo "${res}"
+}
+
 if [[ "$#" -ne 0 ]];
 then
     docker login --username=${DOCKER_USER_NAME} --password=${DOCKER_PASSWORD:-}
@@ -28,10 +40,12 @@ then
 
         if [[ "${variant}" = "full" ]];
         then
-            docker_full_image_name="meterian/cli-canary:$(cat ../version.txt)"
+            image_tag="$(cat ../version.txt)"
+            docker_full_image_name="meterian/cli-canary:${image_tag}"
             docker_full_image_name_latest="meterian/cli-canary:latest"
         else
-            docker_full_image_name="meterian/cli-canary:$(cat variants/${variant}/version.txt)-${variant}"
+            image_tag="$(cat variants/${variant}/version.txt)-${variant}"
+            docker_full_image_name="meterian/cli-canary:${image_tag}"
             docker_full_image_name_latest="meterian/cli-canary:latest-${variant}"
         fi
 
@@ -41,8 +55,13 @@ then
             continue
         else
             echo "Docker image '${docker_full_image_name}' found in the local repository"
+            if [[ ! "$(isImageVersionOnDockerHub ${image_tag})" -eq 0 ]];
+            then
+                pushDockerImage ${image_found} ${docker_full_image_name} ${docker_full_image_name_latest}
+            else
+                echo "Aborting image upload: '${docker_full_image_name}' already exists on docker hub"
+            fi
         fi
-        pushDockerImage ${image_found} ${docker_full_image_name} ${docker_full_image_name_latest}
 
     done
 
