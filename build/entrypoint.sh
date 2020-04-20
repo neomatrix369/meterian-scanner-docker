4a30cc5845782d7e09a91e4edc0fd815f9f9f882
Nana Insaidoo <nana.insaidoo@meterian.com>
2020-04-20 15:17:33 +0100

    Now with Swift and Objective-C with cocoapods support
    - Edited entrypoint.sh so that the scan is run by the meterian user to accommodate a limitation of cocoapods
      that restricts the use of the pod command to only non-root users

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -20,33 +20,33 @@ echo "export GRADLE_USER_HOME=~/.gradle" >> /tmp/init.sh
 echo "export GOROOT=/usr/local/go" >> /tmp/init.sh
 echo "export PATH=\${GOROOT}/bin:\${PATH}" >> /tmp/init.sh
 
-# run the script binding the user if required
-if [ "${HOST_UID}" == "" ];
+# prepare command options with the host uid and gid when present
+WITH_HUID=""
+WITH_HGID=""
+if [ -n "${HOST_UID}" ];
 then
-    /tmp/meterian.sh 2>/dev/null
-
-    # please do not add any command here as we need to preserve the exit status
-    # of the meterian client
-else
-    # create the user
-    groupadd -g ${HOST_GID} -o meterian
-    useradd -g meterian -ou ${HOST_UID} meterian -d /home/meterian
-
-    # creating home dir if it doesn't exist
-    if [ ! -d "/home/meterian" ];
-    then
-        mkdir /home/meterian
-    fi
-
-    #changing home dir group and ownership
-    chown meterian:meterian /home/meterian
-
-    # launch meterian client with the newly created user
-    su meterian -c -m /tmp/meterian.sh  2>/dev/null
-
-    # please do not add any command here as we need to preserve the exit status
-    # of the meterian client
+    WITH_HGID="-g ${HOST_GID} -o"
+    WITH_HUID="-ou ${HOST_UID}"
 fi
+
+# create the user
+groupadd ${WITH_HGID} meterian
+useradd -g meterian ${WITH_HUID} meterian -d /home/meterian
+
+# creating home dir if it doesn't exist
+if [ ! -d "/home/meterian" ];
+then
+    mkdir /home/meterian
+fi
+
+#changing home dir group and ownership
+chown meterian:meterian /home/meterian
+
+# launch meterian client with the newly created user
+su meterian -c -m /tmp/meterian.sh  2>/dev/null
+
+# please do not add any command here as we need to preserve the exit status
+# of the meterian client
 
 # please do not add any command here as we need to preserve the exit status
 # of the meterian client
