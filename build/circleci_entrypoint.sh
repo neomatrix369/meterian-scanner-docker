f4e6c42e9e5670ffa3f5998664409d491041b118
Nana Insaidoo <insaidoo.nana@yahoo.it>
2021-11-24 14:36:36 +0000

    Now with circleci-specific entrypoint script

---
+++ build/circleci_entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+METERIAN_HOME="/home/meterian/"
+mkdir -p ${METERIAN_HOME}
+
+if [[ -f ${HOME}/.gitconfig ]]; then
+    cp ${HOME}/.gitconfig ${METERIAN_HOME}
+fi
+
+if [[ -d ${HOME}/.ssh ]]; then
+   cp -R ${HOME}/.ssh ${METERIAN_HOME}
+fi
+
+if [[ -f ${METERIAN_HOME}.ssh/config ]]; then
+    sed -i 's/IdentityFile.*\.ssh/IdentityFile \/home\/meterian\/.ssh/g' ${METERIAN_HOME}.ssh/config
+fi
+
+sed -i 's/chown/chown -R/g' /root/entrypoint.sh
+/root/entrypoint.sh $*
