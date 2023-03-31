eecce72c3be2b2dc868dc799ee55bd9457694c18
Nana Insaidoo <nana.insaidoo@meterian.com>
2023-03-31 08:35:57 +0100

    Updated meterian_github_action script

--- build/variants/gha/meterian_github_action.sh
+++ build/variants/gha/meterian_github_action.sh
@@ -10,4 +10,9 @@ fi
 curl -sS https://raw.githubusercontent.com/MeterianHQ/meterian-github-action/$VERSION/entrypoint.sh -o /root/entrypoint.sh
 curl -sS https://raw.githubusercontent.com/MeterianHQ/meterian-github-action/$VERSION/meterian.sh -o /root/meterian.sh
 
+currDir=$(pwd)
+UID="$(stat -c '%u' "${currDir}")"
+GID="$(stat -c '%g' "${currDir}")"
+chown -R $UID:$GID .
+
 /root/entrypoint.sh
